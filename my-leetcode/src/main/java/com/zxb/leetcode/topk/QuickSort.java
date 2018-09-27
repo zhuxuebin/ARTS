633f241d73479b439f48983069427843c9bb0eb2
zhuxuebin <zhuxuebin2014@163.com>
2018-09-27 23:55:12 +0800

    TopKAlgo.java

---
+++ my-leetcode/src/main/java/com/zxb/leetcode/topk/QuickSort.java
@@ -0,0 +1,73 @@
+package com.sf.boot.myspringboot.topK;
+
+import java.util.Random;
+
+/**
+ * Created by xuery on 2018/9/22.
+ * https://blog.csdn.net/MoreWindows/article/details/6684558--填坑法
+ */
+public class QuickSort {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        for(int i=0;i<100;i++){
+            arr[i] = random.nextInt(Integer.MAX_VALUE);
+        }
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(arr);
+        for(int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public void quickSort(int[] arr){
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    private void quickSort(int[] arr, int begin, int end){
+        if(begin >= end){
+            return;
+        }
+        int p = partition(arr, begin, end);
+        quickSort(arr, begin, p-1);
+        quickSort(arr, p+1, end);
+    }
+
+    private int partition(int[] arr, int begin, int end){
+        // 1. select random p between begin and end
+        int p = begin + random.nextInt(end+1-begin);
+        // 2. move the element less than arr[p] to p's left, ...
+        int pVal = arr[p];
+        //交换随机选择的p与end，转换成选择最后一个元素为基准
+        swap(arr, p, end);
+        int i = begin, j = end;
+        while(i < j){
+            while(i < j && arr[i] <= pVal){
+                i++;
+            }
+            //填arr[j]这个坑, 大填小（arr[end]为第一个坑）
+            if(i < j){
+                arr[j] = arr[i];
+                j--;
+            }
+            //接下来要填arr[i]这个坑，小填大
+            while(i < j && arr[j] >= pVal){
+                j--;
+            }
+            if(i < j){
+                arr[i] = arr[j];
+                i++;
+            }
+        }
+        arr[i] = pVal;
+        return i;
+    }
+
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ my-leetcode/src/main/java/com/zxb/leetcode/topk/TopKAlgo.java
@@ -0,0 +1,25 @@
+package com.sf.boot.myspringboot.topK;
+
+/**
+ * Created by xuery on 2018/9/22.
+ * 给定一个n个数字大小的数组，求k个最大的元素
+ * 1. 排序之后求O（nlogn）
+ * 2. 冒泡排序，每次遍历算出一个值，O（kn）
+ * 3. 堆排，O（nlogk）
+ * 4. 快排，归并法，O(nlogn)
+ * 5. 减治法
+ */
+public class TopKAlgo {
+
+    //减治法的相关代码
+    public static void main(String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("run");
+            }
+        });
+        t.start();
+        t.start();
+    }
+}

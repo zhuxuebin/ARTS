6efe3633be78290d7c66a2c113342e19166eabbb
xuery <zhuxuebin@sf-express.com>
2019-01-07 20:58:44 +0800

    全排列+左右旋

---
+++ my-leetcode/src/main/java/com/zxb/structurealgo/readblacktree/LeftRightTurn.java
@@ -0,0 +1,11 @@
+package com.zxb.structurealgo.readblacktree;
+
+/**
+ * @ClassName LeftRightTurn
+ * @Description 左右旋操作
+ * @Author xuery
+ * @Date 2019/1/7 19:15
+ * @Version 1.0
+ */
+public class LeftRightTurn {
+}
---
+++ my-leetcode/src/main/java/com/zxb/structurealgo/recursion/AllPermutation.java
@@ -0,0 +1,11 @@
+package com.zxb.structurealgo.recursion;
+
+/**
+ * @ClassName AllPermutation
+ * @Description 全排列
+ * @Author xuery
+ * @Date 2019/1/7 19:17
+ * @Version 1.0
+ */
+public class AllPermutation {
+}

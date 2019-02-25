517cb553b3f2e5dfaf0124515c66bbe36861bc5e
xuery <zhuxuebin@sf-express.com>
2019-02-25 21:11:04 +0800

    greedy算法

---
+++ my-leetcode/src/main/java/com/zxb/structurealgo/greedy/HuffmanCoding.java
@@ -0,0 +1,12 @@
+package com.zxb.structurealgo.greedy;
+
+/**
+ * @ClassName HuffmanCoding
+ * @Description 哈弗曼编码的实现
+ * 借助优先级队列，构造出二叉树，然后遍历二叉树得到编码（怎么遍历是个问题）
+ * @Author xuery
+ * @Date 2019/2/25 21:09
+ * @Version 1.0
+ */
+public class HuffmanCoding {
+}
---
+++ my-leetcode/src/main/java/com/zxb/structurealgo/greedy/MaxSectionCount.java
@@ -0,0 +1,11 @@
+package com.zxb.structurealgo.greedy;
+
+/**
+ * @ClassName MaxSectionCount
+ * @Description 给定多个区间，求不相交的区间的最大个数，边界值相等不算相交
+ * @Author xuery
+ * @Date 2019/2/25 21:07
+ * @Version 1.0
+ */
+public class MaxSectionCount {
+}
